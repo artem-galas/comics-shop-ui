7e12d3f24d00427cf136010a579be5717ba16ee7
Artem Halas <artem.halas@pipedrive.com>
2020-02-21 21:36:33 +0200

    Initial application packages

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    '@typescript-eslint',
+    "react-hooks"
+  ],
+  extends: [
+    "airbnb",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  rules: {
+    "react/jsx-filename-extension": ["warn", {
+      "extensions": [".jsx", ".tsx"]
+    }],
+    "@typescript-eslint/prefer-interface": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react/jsx-props-no-spreading": ["warn", {
+      "html": "ignore",
+      "exceptions": ["Component"],
+    }],
+    "react/prop-types": "off",
+    "import/prefer-default-export": "off",
+    "import/named": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/extensions": ["off"],
+    "import/no-unresolved": ["off"],
+    "@typescript-eslint/unbound-method": [ "off"],
+    "object-curly-newline": ["error", {
+      "ObjectExpression": { "multiline": true },
+      "ObjectPattern": { "multiline": true },
+      "ImportDeclaration": { "multiline": true, "minProperties": 5 },
+      "ExportDeclaration": { "multiline": true, "minProperties": 3 }
+    }]
+  }
+};
